52e10ee3c9247bbd8433017439839c466736a423
RoshanAswal <73030476+RoshanAswal@users.noreply.github.com>
2022-03-03 21:25:47 +0530

    Time: 89 ms (69.98%), Space: 30.2 MB (50.01%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)q.push({i,j});
+                else mat[i][j]=-1;
+            }
+        }
+        while(!q.empty()){
+            int i=q.front().first,j=q.front().second;
+            q.pop();
+            if(i-1>=0 && mat[i-1][j]==-1){
+                mat[i-1][j]=mat[i][j]+1;
+                q.push({i-1,j});
+            }
+            if(i+1<n && mat[i+1][j]==-1){
+                mat[i+1][j]=mat[i][j]+1;
+                q.push({i+1,j});
+            }
+            if(j-1>=0 && mat[i][j-1]==-1){
+                mat[i][j-1]=mat[i][j]+1;
+                q.push({i,j-1});
+            }
+            if(j+1<m && mat[i][j+1]==-1){
+                mat[i][j+1]=mat[i][j]+1;
+                q.push({i,j+1});
+            }
+        }
+        return mat;
+    }
+    
+};
