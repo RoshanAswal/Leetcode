2d371ae000168f11d5e931de7b39016550a6d88a
RoshanAswal <73030476+RoshanAswal@users.noreply.github.com>
2022-03-28 22:11:35 +0530

    Time: 125 ms (46.06%), Space: 14.3 MB (70.87%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    
-    int coin(int i,int amt,vector<int> &coins,vector<vector<int>> &dp){
-       if(amt==0) return 0;
-        if(i>=coins.size()) return INT_MAX/2;
-        if(dp[amt][i]!=-1) return dp[amt][i];
-        
-        if(coins[i]<=amt) return dp[amt][i] =min({1+coin(i,amt-coins[i],coins,dp),coin(i+1,amt,coins,dp)});
-        else return dp[amt][i]=coin(i+1,amt,coins,dp);
-        
-    }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(amount+3,vector<int>(coins.size()+2,-1));
-        int ans = coin(0,amount,coins,dp);
-        
-        if(ans>=INT_MAX/2) return -1;
-        return ans;
-        
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(coins[j]<=i)
+                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
+            }
+        }        
+        if(dp[amount]==amount+1)return -1;
+        return dp[amount];
     }
 };
