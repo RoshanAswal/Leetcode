0f6ddbb92899e74e3fb59cd47fe673071a285d8c
RoshanAswal <73030476+RoshanAswal@users.noreply.github.com>
2022-04-11 18:34:44 +0530

    Time: 172 ms (65.64%), Space: 11.2 MB (77.97%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool sol(int i,int t,vector<int>& nums,int sum,vector<vector<int>> &dp){
-        if(i>=nums.size() || t>sum/2)return false;
-        if(sum/2==t)return true;
-        if(dp[i][t]!=-1)return dp[i][t];
-        bool f=sol(i+1,t+nums[i],nums,sum,dp);
-        bool s=sol(i+1,t,nums,sum,dp);
-        if(f || s)
-            dp[i][t]=true;
-        else
-            dp[i][t]=false;
-        return dp[i][t];
-    }
     bool canPartition(vector<int>& nums) {
+    // Just like subset sum if we find the half sum then it is guarantee that other half is present
         int sum=0;
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];
+        for(int i:nums)sum+=i;
         if(sum&1)return false;
-        vector<vector<int>> dp(nums.size(),vector<int>(sum/2+1,-1));
-        return sol(0,0,nums,sum,dp);
+        bool dp[nums.size()+1][sum/2+1];
+        for(int i=0;i<=nums.size();i++)
+            dp[i][0]=true;
+        for(int i=1;i<=sum/2;i++)
+            dp[0][i]=false;
+        for(int i=1;i<=nums.size();i++){
+            for(int j=1;j<=sum/2;j++){
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[nums.size()][sum/2];
     }
 };
