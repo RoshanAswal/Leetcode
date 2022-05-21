e1381f2ad7a6272374c3d42eed90c771d7e98745
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-05-21 22:05:06 +0530

    Time: 37 ms (16.92%), Space: 7 MB (53.79%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -7,18 +7,15 @@ public:
         for(char c ='A';c<='Z';c++){
             int i=0;
             int last=0;
-            int cnt=0;
             int k = k2;
-            // cout<<c<<" ";
             while(i<s.size()){
-                if(s[i]==c) {i++,cnt++;}
-                else if(k) {k--,i++,cnt++;}
+                if(s[i]==c) i++;
+                else if(k) k--,i++;
                 else{
                    if(s[last]!=c) k++;
-                    cnt--;
                     last++;
                 }
-                ans=max(ans,cnt);
+                ans=max(ans,i-last);
             }
         }
         return ans;
