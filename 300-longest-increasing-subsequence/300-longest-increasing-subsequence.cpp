d1a75d1de37ae328f84943802ec4f9ea7969b392
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-05-25 12:52:13 +0530

    Time: 461 ms (24.27%), Space: 10.5 MB (67.17%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int dp[2501];
-    Solution(){memset(dp,-1,sizeof dp);}
-    int sol(int n,vector<int> &nums,int prev){
-        if(n>=nums.size())return 0;
-        if(dp[prev+1]!=-1)return dp[prev+1];
-        int no=sol(n+1,nums,prev),yes=0;
-        if(prev==-1 || nums[n]>nums[prev])
-            yes=1+sol(n+1,nums,n);
-        return dp[prev+1]=max(no,yes);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        return sol(0,nums,-1);
+        int n=nums.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j])
+                    dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
