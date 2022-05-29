60de2ce6b92112d68df3ac2f34bfdbd2bbafdda5
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-05-29 16:00:38 +0530

    Time: 142 ms (35.48%), Space: 21.1 MB (24.42%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool check(int n,int m,vector<vector<int>> &st){
-        for(int i=0;i<26;i++){
-            if(st[n][i] && st[m][i])return false;
-        }
-        return true;
-    }
     int maxProduct(vector<string>& words) {
-        vector<vector<int>> st;
+        vector<int> st;
         for(int i=0;i<words.size();i++){
             vector<int> temp(26,0);
-            for(char &c:words[i])
-                temp[c-'a']=1;
-            st.push_back(temp);
+            int res=0;
+            for(char &c:words[i])   temp[c-'a']+=1;
+            for(int j=0;j<26;j++){
+                if(temp[j]) res|=(1<<j);
+            }
+            st.push_back(res);
         }
         int ans=0;
         for(int i=0;i<words.size();i++){
-            char c=words[i][0];
             int l1=words[i].length();
             for(int j=i+1;j<words.size();j++){
                 int l2=words[j].length();
-                if(check(i,j,st)){
+                if((st[i]&st[j])==0)
                     ans=max(ans,l1*l2);
-                }
             }
         }
         return ans;
