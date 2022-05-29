5ea1df2c74c1adb31e84c1fb735df67172a80d5b
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-05-29 14:03:14 +0530

    Time: 253 ms (23.27%), Space: 29.2 MB (13.88%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(int n,int m,vector<vector<int>> &st){
+        for(int i=0;i<26;i++){
+            if(st[n][i] && st[m][i])return false;
+        }
+        return true;
+    }
+    int maxProduct(vector<string>& words) {
+        vector<vector<int>> st;
+        sort(words.begin(),words.end());
+        for(int i=0;i<words.size();i++){
+            vector<int> temp(26,0);
+            for(char &c:words[i])
+                temp[c-'a']=1;
+            st.push_back(temp);
+            string s=words[i];
+            sort(s.begin(),s.end());
+            words[i]=s;
+        }
+        int ans=0;
+        for(int i=0;i<words.size();i++){
+            char c=words[i][0];
+            int l1=words[i].length();
+            for(int j=i+1;j<words.size();j++){
+                int l2=words[j].length();
+                if(check(i,j,st)){
+                    // cout<<"yes"<<endl;
+                    ans=max(ans,l1*l2);
+                }
+            }
+        }
+        return ans;
+    }
+};
