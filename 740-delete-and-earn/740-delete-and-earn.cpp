7a6caf7e4e9dbfbfaa9c0ec11d7b0243a10f260d
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-06-10 10:13:56 +0530

    Time: 11 ms (90.23%), Space: 11.7 MB (87.38%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        map<int,int> m;
-        int mx=*max_element(nums.begin(),nums.end());
-        for(int i=1;i<=mx;i++)m[i]=0;
-        for(int i=0;i<nums.size();i++)m[nums[i]]+=nums[i];
-        int in=0,out=0;
-        for(int j=1;j<=mx;j++){
-            int i=out+m[j];
-            int o=max(in,out);
-            in=i;
-            out=o;
+        int mx=0;
+        for(int &i:nums)mx=max(i,mx);
+        vector<int> dp(mx+1,0);
+        for(int &i:nums)dp[i]+=i;
+        for(int i=2;i<dp.size();i++){
+            dp[i]=max(dp[i-1],dp[i]+dp[i-2]);
         }
-        return max(out,in);
+        return dp[dp.size()-1];
     }
 };
