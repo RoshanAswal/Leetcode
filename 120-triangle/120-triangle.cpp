e1bb28b12fa4f1338273a1541b7d68194a0385ee
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-06-13 12:39:09 +0530

    Time: 6 ms (80.20%), Space: 9.1 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int sol(vector<vector<int>> &t,int i,int j,vector<vector<int>> &dp){
-        if(i==t.size()-1)return t[i][j];
-        if(dp[i][j]!=-1)return dp[i][j];
-        dp[i][j]=min(sol(t,i+1,j,dp),sol(t,i+1,j+1,dp))+t[i][j];
-        return dp[i][j];
-    }
     int minimumTotal(vector<vector<int>>& t) {
         int n=t.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return sol(t,0,0,dp);
+        for(int i=0;i<n;i++)dp[n-1][i]=t[n-1][i];
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<t[i].size();j++)
+                dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+t[i][j];
+        }
+        return dp[0][0];
     }
 };
