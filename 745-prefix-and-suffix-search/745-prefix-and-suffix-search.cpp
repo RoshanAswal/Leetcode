4b6c1cc9704e4052073dbcef1d773c0dad27331f
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-06-18 19:46:47 +0530

    Time: 742 ms (49.26%), Space: 138.8 MB (75.37%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,41 @@
+class TrieNode {
+public:
+    map<int,TrieNode*> children;
+    int weight;
+     TrieNode() {
+        // children = new TrieNode[27];
+        // for(int i=0;i<27;i++) children[i] = NULL;
+        weight = 0;
+    }
+};
+class WordFilter {
+public:
+    TrieNode *trie;
+     WordFilter(vector<string> &words) {
+        trie = new TrieNode();
+        for (int weight = 0; weight < words.size(); ++weight) {
+            string word = words[weight] + "{";
+            for (int i = 0; i < word.length(); ++i) {
+                TrieNode *cur = trie;
+                cur->weight = weight;
+                for (int j = i; j < 2 * word.length() - 1; ++j) {
+                    int k = word[j % word.length()] - 'a';
+                    if (cur->children.count(k) == 0)
+                        cur->children[k] = new TrieNode();
+                    cur = cur->children[k];
+                    cur->weight = weight;
+                }
+            }
+        }
+    }
+     int f(string prefix, string suffix) {
+        TrieNode *cur = trie;
+        suffix+='{';
+        suffix+=prefix;
+        for (char &letter:suffix) {
+            if (cur->children.count(letter - 'a') == 0) return -1;
+            cur = cur->children[letter - 'a'];
+        }
+        return cur->weight;
+    }
+};
