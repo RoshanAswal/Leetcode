da0e0f5c0bbf0c0734b666476823d8053834c50a
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-07 17:07:53 +0530

    Time: 798 ms (14.42%), Space: 6.1 MB (99.90%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int dp[2001];
     bool ispali(string &s,int l,int r){
         if(l>=r)return true;
         if(s[l]!=s[r])return false;
         return ispali(s,l+1,r-1);
     }
-    int sol(string s,int i){
-        if(i==s.length())return 0;
-        if(dp[i]!=-1)return dp[i];
-        string st;
-        int ans=INT_MAX;
-        for(int j=i;j<s.length();j++){
-            st+=s[j];
-            if(ispali(st,0,st.length()-1)){
-                int cnt=sol(s,j+1)+1;
-                ans=min(ans,cnt);
-            }
-        }
-        return dp[i]=ans;
-    }
     int minCut(string s) {
-        memset(dp,-1,sizeof(dp));
-        int ans=sol(s,0);
-        return ans-1;
+        int dp[s.length()+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=s.length()-1;i>=0;i--){
+            int ans=INT_MAX;
+            for(int j=i;j<s.length();j++){
+                if(ispali(s,i,j)){
+                    int cnt=dp[j+1]+1;
+                    ans=min(ans,cnt);
+                }
+            }
+            dp[i]=ans;
+        }
+        return dp[0]-1;
     }
 };
