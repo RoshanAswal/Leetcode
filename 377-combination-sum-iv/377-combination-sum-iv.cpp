2c4282fbc5a6deba7999865b9b117102e51be9b1
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-08 09:44:31 +0530

    Time: 5 ms (33.70%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int dp[1001];
-    int sol(vector<int> &nums,int tar){
-        if(tar==0)return 1;
-        if(dp[tar]!=-1)return dp[tar];
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<=tar)
-                ans+=sol(nums,tar-nums[i]);
-        }
-        return dp[tar]=ans;
-    }
     int combinationSum4(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        return sol(nums,target);
+        unsigned dp[target+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]<=i)
+                    dp[i]+=dp[i-nums[j]];
+            }
+        }
+        return dp[target];
     }
 };
