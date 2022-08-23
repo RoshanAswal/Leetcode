8a7f850d3ab416f3f5fc8e9d106b1949976d5e21
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-23 07:46:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.36%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int dp[101][101];
-    // int sol(int m,int n,int i,int j){
-    //     if(i<0 || j<0 || i>=m || j>=n)return 0;
-    //     if(i==m-1 && j==n-1)return 1;
-    //     if(dp[i][j]!=-1)return dp[i][j];
-    //     int a=sol(m,n,i+1,j);
-    //     int b=sol(m,n,i,j+1);
-    //     dp[i][j]=a+b;
-    //     return dp[i][j];        
-    // }
+    Solution(){memset(dp,0,sizeof(dp));}
     int uniquePaths(int m, int n) {
-        if(n==1 && m==1)return 1;
-        memset(dp,0,sizeof(dp));
-        dp[0][0]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]+=dp[i-1][j]+dp[i][j-1]+1;
-            }
-        }
-        return dp[m-1][n-1]+1;
+        if(m==1 && n==1)return 1;
+        if(dp[m][n]!=0)return dp[m][n];
+        if(m-1>0)dp[m][n]+=uniquePaths(m-1,n);
+        if(n-1>0)dp[m][n]+=uniquePaths(m,n-1);
+        return dp[m][n];
     }
 };
