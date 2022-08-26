5e17ffe4adf3400c6f65e056770ad39d0454c1a9
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-26 15:25:34 +0530

    Time: 2876 ms (5.05%), Space: 173.7 MB (5.04%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -8,20 +8,18 @@ public:
         q.push({0,0});
         while(!q.empty()){
             int s=q.size();
-            while(s--){
-                auto it=q.front();
-                q.pop();
-                int l=it.first,r=it.second;
-                if(l+r==t || l==t || r==t)return true;
-                if(vis[{l,r}])continue;
-                vis[{l,r}]=1;
-                q.push({0,j2});q.push({j1,0});
-                if(l<=j2-r)q.push({0,min(j2,l+r)});
-                else q.push({l-(j2-r),min(j2,l+r)});
-                if(r<=j1-l)q.push({min(j1,l+r),0});
-                else q.push({min(j2,l+r),r-(j1-l)});
-                q.push({l,j2});q.push({j1,r});
-            }
+            auto it=q.front();
+            q.pop();
+            int l=it.first,r=it.second;
+            if(l+r==t || l==t || r==t)return true;
+            if(vis[{l,r}])continue;
+            vis[{l,r}]=1;
+            q.push({0,j2});q.push({j1,0});
+            if(l<=j2-r)q.push({0,min(j2,l+r)});
+            else q.push({l-(j2-r),min(j2,l+r)});
+            if(r<=j1-l)q.push({min(j1,l+r),0});
+            else q.push({min(j2,l+r),r-(j1-l)});
+            q.push({l,j2});q.push({j1,r});
         }
         return false;
     }
