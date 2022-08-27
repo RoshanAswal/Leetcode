9b5dce58d09b1db82d950e4d0336141c0a88d71b
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-27 18:06:29 +0530

    Time: 1734 ms (54.67%), Space: 293.4 MB (42.82%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& mat, int tar) {
-        int row=mat.size(),col=mat[0].size();
-        int arr[row+1][col+1];
-        memset(arr,0,sizeof(arr));
-        int ans=INT_MIN;
-        for(int i=1;i<=row;i++){
-            for(int j=1;j<=col;j++){   
-                if(mat[i-1][j-1]==tar)return tar;
-                else if(mat[i-1][j-1]<tar)ans=max(ans,mat[i-1][j-1]);
-                arr[i][j]=(arr[i-1][j]+arr[i][j-1]-arr[i-1][j-1]+mat[i-1][j-1]);
-            }   
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        int row=matrix.size(),col=matrix[0].size();
+        int arr[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]=matrix[i][j];
+            }
         }
-        for(int i=0;i<=row;i++){
-            for(int j=0;j<=col;j++){
-                for(int k=i+1;k<=row;k++){
-                    for(int l=j+1;l<=col;l++){
-                        int temp=arr[k][l]-arr[k][j]-arr[i][l]+arr[i][j];
-                        if(temp==tar)return tar;
-                        else if(temp<tar)ans=max(ans,temp);
-                    }
+        int ans=INT_MIN;
+        for(int l=0;l<col;l++){
+            int sum[row];
+            memset(sum,0,sizeof(sum));
+            for(int r=l;r<col;r++){
+                for(int i=0;i<row;i++){
+                    sum[i]+=arr[i][r];
                 }
+                set<int> s;
+                s.insert(0);
+                int currsum=0,temp=INT_MIN;
+                for(int &i:sum){
+                    currsum+=i;
+                    set<int>::iterator it=s.lower_bound(currsum-k);
+                    if(it!=s.end()){
+                        temp=max(temp,currsum-*it);
+                    }
+                    s.insert(currsum);
+                }
+                ans=max(ans,temp);
             }
         }
         return ans;
