fe2f484a62063b840a75813016f3cf4dacea7184
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-29 08:23:23 +0530

    Time: 125 ms (7.34%), Space: 21.2 MB (7.30%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    void sol(vector<vector<char>> &grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!='1')
-            return;
-        grid[i][j]='2';
-        sol(grid,i+1,j);
-        sol(grid,i-1,j);
-        sol(grid,i,j+1);
-        sol(grid,i,j-1);
-    }
     int numIslands(vector<vector<char>>& grid) {
+        int row=grid.size(),col=grid[0].size();
         int ans=0;
-        int m=grid.size(),n=grid[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    sol(grid,i,j);
-                    ans++;
+        vector<vector<int>> vis(row,vector<int>(col,0));
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(vis[i][j]==1)continue;
+                if(grid[i][j]=='0'){vis[i][j]=1;continue;}
+                queue<pair<int,int>> q;
+                q.push({i,j});
+                while(!q.empty()){
+                    int s=q.size();
+                    for(int k=0;k<s;k++){
+                        auto it=q.front();
+                        q.pop();
+                        int x=it.first,y=it.second;
+                        if(vis[x][y]==1)continue;
+                        vis[x][y]=1;
+                        if(grid[x][y]=='0')continue;
+                        if(x+1<row)q.push({x+1,y});
+                        if(x-1>=0)q.push({x-1,y});
+                        if(y+1<col)q.push({x,y+1});
+                        if(y-1>=0)q.push({x,y-1});
+                    }
                 }
+                ans++;
             }
         }
         return ans;
