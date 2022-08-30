f02d6c8eddf6fa68c4527affb4368d65effb6fa8
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-30 08:12:52 +0530

    Time: 6 ms (23.35%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp;
         int row=matrix.size(),col=matrix[0].size();
-        for(int j=0;j<col;j++){
-            vector<int> ex;
-            for(int i=row-1;i>=0;i--){
-                ex.push_back(matrix[i][j]);
+        int f1=-1,f2=-1,s1=row,s2=-1,t1=row,t2=col,fo1=-1,fo2=col;
+        while(1){
+            f1++;f2++;s1--;s2++;t1--;t2--;fo1++;fo2--;
+            int tf1=f1,tf2=f2,ts1=s1,ts2=s2,tt1=t1,tt2=t2,tfo1=fo1,tfo2=fo2;
+            int cnt=s1-f1,cntt=cnt;
+            while(cntt && tf1<row-1){
+                swap(matrix[tf1][tf2],matrix[ts1][ts2]);
+                swap(matrix[ts1][ts2],matrix[tt1][tt2]);
+                swap(matrix[tt1][tt2],matrix[tfo1][tfo2]);
+                tf1++;ts2++;
+                tt1--;tfo2--;
+                cntt--;
             }
-            temp.push_back(ex);
+            if(cnt<=1)break;
         }
-        matrix=temp;
+        
     }
 };
