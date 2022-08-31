75407012e38e3f7b93b29f4a6969bd55156324cf
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-08-31 10:36:50 +0530

    Time: 46 ms (91.63%), Space: 19.1 MB (49.23%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void dfs(int i,int j,int m,int n,vector<vector<int>> &heights,vector<vector<int>> &v,int preval){
-        if(i<0 || j<0 || i>=m || j>=n || heights[i][j]<preval || v[i][j]==1)return;
-        v[i][j]=1;
-        dfs(i+1,j,m,n,heights,v,heights[i][j]);
-        dfs(i-1,j,m,n,heights,v,heights[i][j]);
-        dfs(i,j+1,m,n,heights,v,heights[i][j]);
-        dfs(i,j-1,m,n,heights,v,heights[i][j]);
+    void dfs(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &vis,int prev){
+        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size() || mat[i][j]<prev || vis[i][j]==1)return;
+        vis[i][j]=1;
+        dfs(i+1,j,mat,vis,mat[i][j]);
+        dfs(i-1,j,mat,vis,mat[i][j]);
+        dfs(i,j+1,mat,vis,mat[i][j]);
+        dfs(i,j-1,mat,vis,mat[i][j]);
     }
+    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m=heights.size(),n=heights[0].size();
-        if(n==0)return {};
-        vector<vector<int>> P(m,vector<int>(n,0));
-        vector<vector<int>> A(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            dfs(i,0,m,n,heights,P,-1);
-            dfs(i,n-1,m,n,heights,A,-1);
+        int row=heights.size(),col=heights[0].size();
+        vector<vector<int>> l(row,vector<int>(col,0)),r(row,vector<int>(col,0));
+        for(int i=0;i<row;i++){
+            dfs(i,0,heights,l,-1);
+            dfs(i,col-1,heights,r,-1);
         }
-        for(int i=0;i<n;i++){
-            dfs(0,i,m,n,heights,P,-1);
-            dfs(m-1,i,m,n,heights,A,-1);
+        for(int i=0;i<col;i++){
+            dfs(0,i,heights,l,-1);
+            dfs(row-1,i,heights,r,-1);
         }
         vector<vector<int>> ans;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(P[i][j]==1 && A[i][j]==1)
-                    ans.push_back(vector<int>{i,j});
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(l[i][j]==1 && r[i][j]==1){
+                    ans.push_back({i,j});
+                }
             }
         }
         return ans;
