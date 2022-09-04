0110e9e6c148d38e07e1c8850b05c28a6f5edb2d
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-09-04 11:40:08 +0530

    Time: 526 ms (50.00%), Space: 45.7 MB (50.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,7 +1,7 @@
-int dp[5005][5005];
-int mod=1e9+7;
 class Solution {
 public:
+    int mod=1e9+7;
+    int dp[3005][3005];
     int sol(int s,int e,int k){
         if(k==0)return s==e;
         if(dp[s][k]!=-1)return dp[s][k];
@@ -11,6 +11,6 @@ public:
     }
     int numberOfWays(int s, int e, int k) {
         memset(dp,-1,sizeof(dp));
-        return sol(s+2000,e+2000,k);
+        return sol(s+1000,e+1000,k);
     }
 };
