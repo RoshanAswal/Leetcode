3caf990d1d4b3d3661171cb778ed24f1297ca4bd
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-09-08 08:06:39 +0530

    Time: 54 ms (25.41%), Space: 10.1 MB (34.71%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l=0,r=0,res=-1;
         map<char,int> m;
-        string temp;
-        while(r<s.length()){
-            if(m.find(s[r])!=m.end() && m[s[r]]!=-1){
-                while(l<=m[s[r]]){
-                    m[s[l]]=-1;
-                    l++;
-                }
+        int ans=0,prev=0,l=s.length();
+        for(int i=0;i<l;i++){
+            if(m.count(s[i])==0)m[s[i]]=i;
+            else{
+                ans=max(ans,i-prev);
+                for(int j=prev;j<m[s[i]];j++)
+                    m.erase(s[j]);
+                prev=m[s[i]]+1;
+                m[s[i]]=i;
             }
-            res=max(res,r-l+1);
-            m[s[r]]=r;
-            r++;
         }
-        return res==-1?s.length():res;
+        ans=max(l-prev,ans);
+        if(ans==0)return s.length();
+        return ans;
     }
 };
