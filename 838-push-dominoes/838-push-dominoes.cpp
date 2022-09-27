40fbdedad321464712f2c4b450be4241e46156c6
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-09-27 18:47:38 +0530

    Time: 311 ms (5.72%), Space: 21.1 MB (36.05%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,50 +1,40 @@
 class Solution {
 public:
     string pushDominoes(string dom) {
-        vector<int> v;   
-        for(char &c:dom){
-            if(c=='.')v.push_back(0);
-            if(c=='L')v.push_back(-1);
-            if(c=='R')v.push_back(1);
-        }
-        vector<int> v1(v.begin(),v.end());
-        vector<int> v2(v.begin(),v.end());
-        int prev=0;
-        for(int i=0;i<v.size();i++){
-            if(v1[i]==0 && prev==1)v1[i]=1;
-            if(v1[i]==1)prev=1;
-            if(v1[i]<0)prev=0;
-        }
-        prev=0;
-        for(int i=v.size()-1;i>=0;i--){
-            if(v2[i]==0 && prev==-1)v2[i]=-1;
-            if(v2[i]==-1)prev=-1;
-            if(v2[i]>0)prev=0;
-        }
-        for(int i=0;i<dom.length();i++){
-            if(!v1[i] && !v2[i])v[i]=5;
-            else v[i]=v1[i]+v2[i];   
-        }
         map<int,int> m;
-        for(int i=0;i<v.size();i++){
-            int ind=i;
-            if(v[i]!=0){
-                if(v[i]==5)dom[i]='.';
-                else if(v[i]>0)dom[i]='R';
-                else dom[i]='L';
-            }
-            while(ind>0 && ind<v.size() && !v[ind])ind++;
-            if(ind<v.size() && ind!=i){
-                m[i]=ind-1;i=ind;
-            }
+        int i=0;
+        while(i<dom.size()){
+            if(dom[i]=='R'){
+                i++;
+                int ind=i;
+                while(i<dom.size() && dom[i]=='.')i++;
+                if(i<dom.size() && dom[i]=='L')m[ind]=i-1;
+            }else i++;
         }
-        for(auto it:m){
+        i=dom.size()-1;
+        while(i>=0){
+            if(!m.count(i) && dom[i]=='L'){
+                i--;
+                while(i>=0 && dom[i]=='.'){
+                    dom[i]='L';i--;
+                }
+            }else i--;
+        }
+        i=0;
+        while(i<dom.size()){
+            if(!m.count(i) && dom[i]=='R'){
+                i++;
+                while(i<dom.size() && dom[i]=='.'){
+                    dom[i]='R';i++;
+                }
+            }else i++;
+        }
+        for(auto &it:m){
             int l=it.first,r=it.second;
             while(l<=r){
                 if(l==r)dom[l]='.';
                 else{
-                    dom[l]='R';
-                    dom[r]='L';
+                    dom[l]='R';dom[r]='L';
                 }
                 l++;r--;
             }
