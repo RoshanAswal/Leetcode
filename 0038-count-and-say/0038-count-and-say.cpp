f0490f5aa9ede86fa96dcc5449d6ab5d549ddb8a
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-10-18 21:56:46 +0530

    Time: 17 ms (49.52%), Space: 7 MB (33.04%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string sol(string temp,int n,int num){
         if(num>=n)return temp;
-        vector<pair<char,int>> p;
         int i=0,l=temp.length();
         string ans;
         while(i<l){
@@ -11,16 +10,12 @@ public:
             while(i<l && temp[i]==c){
                 i++;cnt++;
             }
-            p.push_back({c,cnt});
-        }
-        for(int i=0;i<p.size();i++){
-            ans+=to_string(p[i].second);
-            ans+=p[i].first;
+            ans+=to_string(cnt);
+            ans+=c;
         }
         return sol(ans,n,num+1);
     }
     string countAndSay(int n) {
-        if(n==1)return "1";
         return sol("1",n,1);
     }
 };
