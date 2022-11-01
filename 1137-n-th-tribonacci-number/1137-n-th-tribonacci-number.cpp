6509eee1bcc314022accd162302766fcec2c03da
Roshan Aswal <73030476+RoshanAswal@users.noreply.github.com>
2022-11-01 21:17:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (52.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int sol(int n,vector<int> &dp){
-        if(n<=0)return 0;
-        if(n==1 || n==2)return 1;
+    int dp[38];
+    int sol(int n){
+        if(n==0)return 0;
+        if(n<=2)return 1;
         if(dp[n]!=-1)return dp[n];
-        dp[n]=sol(n-1,dp)+sol(n-2,dp)+sol(n-3,dp);
-        return dp[n];
+        return dp[n]=sol(n-1)+sol(n-2)+sol(n-3);
     }
     int tribonacci(int n) {
-        vector<int> dp(38,-1);
-        return sol(n,dp);
+        memset(dp,-1,sizeof(dp));
+        return sol(n);
     }
 };
